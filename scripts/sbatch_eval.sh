34c5d616d5e7a69a434f9d7fe14d8656e83f0e44
PhiCtl <philippine.descourtils@epfl.ch>
2022-03-14 16:12:33 +0100

    start script sbatch

--- scripts/sbatch_eval.sh
+++ scripts/sbatch_eval.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 1
 #SBATCH --mem 48G
-#SBATCH --time 00:30:00
+#SBATCH --time 00:45:00
 #SBATCH --account vita
 #SBATCH --gres gpu:1
 
@@ -11,10 +11,13 @@ python3 src/eval.py \
 	--domain_name cartpole \
 	--task_name swingup \
 	--action_repeat 8 \
-	--mode color_hard \
+	--mode video \
 	--use_inv \
 	--num_shared_layers 8 \
 	--seed 0 \
 	--work_dir logs/cartpole_swingup/inv/0 \
-	--pad_checkpoint 500k
+	--pad_checkpoint 500k \
+	--pad_num_episodes 10 \
+	--threshold 5.0 \
+	--dependent 1
 
