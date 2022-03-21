0be4b9b1a3bb9a8416055d404e1dd235443f4c73
PhiCtl <philippine.descourtils@epfl.ch>
2022-03-21 09:36:47 +0100

    ignored the done boolean in eval to increase number of timesteps per episode and added a max time steps per episode argument for command line call

--- src/eval.py
+++ src/eval.py
@@ -17,6 +17,7 @@ def evaluate(env, agent, args, video, adapt=False):
 	"""Evaluate an agent, optionally adapt using PAD"""
 	episode_rewards = []
 	recorder = AdaptRecorder(args.work_dir, args.mode)
+	max_tsteps = args.episode_timesteps
 
 	for i in tqdm(range(args.pad_num_episodes)):
 		ep_agent = deepcopy(agent) # make a new copy
@@ -40,10 +41,11 @@ def evaluate(env, agent, args, video, adapt=False):
 			# Take step
 			with utils.eval_mode(ep_agent):
 				action = ep_agent.select_action(obs)
-			next_obs, reward, done, _, change = env.step(action, rewards)
+			next_obs, reward, _, _, change = env.step(action, rewards)
 			episode_reward += reward
 			recorder.update(change, reward)
-			
+			done = step >= max_tsteps
+
 			# Make self-supervised update if flag is true
 			if adapt:
 				if args.use_rot: # rotation prediction
