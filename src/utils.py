changed moving average as causality introduced a delay. Window fixed to 3, with 2 values read ahead and switched to nearest mode

src/utils.py
@@ -55,10 +55,10 @@ class AdaptRecorder(object):
         file_name += "_pad.csv" if adapt else "_eval.csv"
         df_tot.to_csv(os.path.join(self._save_dir, file_name))
 
-def moving_average_reward(rewards, current_ep=None, wind_lgth=5):
+def moving_average_reward(rewards, current_ep=None, wind_lgth=3):
     # Causal convolutional filter
-    w = np.concatenate((np.zeros(wind_lgth -1), np.ones(wind_lgth))).astype(np.float64) / wind_lgth
-    avg = convolve1d(rewards, w, mode='constant')
+    w = np.concatenate((np.zeros(wind_lgth -3), np.ones(wind_lgth+2))).astype(np.float64) / (wind_lgth+2)
+    avg = convolve1d(rewards, w, mode='nearest')
     if current_ep is None:
         return avg
     else: