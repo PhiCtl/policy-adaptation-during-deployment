0df97b805c82cdb3d53e4619816227d4df11228c
PhiCtl <philippine.descourtils@epfl.ch>
2022-03-22 10:28:36 +0100

    reestablished color change

--- src/env/wrappers.py
+++ src/env/wrappers.py
@@ -52,7 +52,7 @@ def shift_hue(x, f=0.5) :
 	assert x.dtype == np.uint8, 'inputs must be uint8 arrays'
 
 	im = TF.to_pil_image(torch.ByteTensor(x))
-	img = TF.adjust_hue(im, 0)
+	img = TF.adjust_hue(im, f)
 	out = np.moveaxis(np.array(img), -1, 0)[:3]
 
 	return out
@@ -299,7 +299,7 @@ class GreenScreen(gym.Wrapper):
 			if avg_reward > self._threshold and self._mode in {'color_easy', 'color_hard'} :
 				self._hue_shift = np.abs(self._hue_shift - 0.5)
 				obs = shift_hue(obs, f=self._hue_shift) # complementary colors
-				#self._change += 1
+				self._change += 1
 		self._current_frame += 1
 		return self._greenscreen(obs), reward, done, info, self._change
 	
