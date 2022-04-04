8a5d4438160d1729912c982950909f1f458e5967
PhiCtl <philippine.descourtils@epfl.ch>
2022-04-04 18:14:18 +0200

    prepared script for cheetah run training

--- scripts/sbatch_train.sh
+++ scripts/sbatch_train.sh
@@ -8,12 +8,16 @@
 #SBATCH --gres gpu:1
 
 python3 src/train.py \
-    --domain_name walker \
-    --task_name walk \
+    --domain_name cheetah \
+    --task_name run \
     --action_repeat 4 \
     --mode train \
     --use_inv \
     --num_shared_layers 8 \
+    --actor_lr 3e-4 \
+    --critic_lr 3e-4 \
+    --encoder_lr 3e-4 \
+    --ss_lr 3e-4 \
     --seed 0 \
-    --work_dir logs/walker_walk/inv/0 \
+    --work_dir logs/cheetah_run/inv/0 \
     --save_model
