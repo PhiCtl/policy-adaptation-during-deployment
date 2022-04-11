9f9af4b0406767f00f380c190ea03fba25157862
PhiCtl <philippine.descourtils@epfl.ch>
2022-04-11 15:41:26 +0200

    new data for video frame1 and solved changing too fast background

--- src/env/wrappers.py
+++ src/env/wrappers.py
@@ -284,6 +284,7 @@ class GreenScreen(gym.Wrapper):
 		self._window = window
 		self._speed = speed
 		self._change = 0
+		self._has_changed = 0
 		self._params = {"b" : 1.0, "h" : 0.0, "c" : 1.0 }
 		self._current_frame = 0 # When speed is left unchanged to 1, is equivalent to steps we take
 		self._video = None
@@ -335,6 +336,7 @@ class GreenScreen(gym.Wrapper):
 		self._current_frame = 0
 		self._params = {"b" : 1.0, "h" : 0.0, "c" : 1.0 }
 		self._change = 0
+		self._has_changed = 0
 		return self._greenscreen(self.env.reset())
 
 	def step(self, action, rewards = None):
@@ -346,8 +348,11 @@ class GreenScreen(gym.Wrapper):
 			avg_reward = moving_average_reward(rewards, current_ep=len(rewards) -1, wind_lgth=self._window)
 
 			if 'steady' in self._mode and self._dependent: # set the frequency of the background shift
-				if avg_reward > self._threshold:
+				if avg_reward > self._threshold and self._has_changed >= self._window:
 					self._change_background()
+					self._has_changed = 0
+				else :
+					self._has_changed += 1
 
 			if 'steady' in self._mode and self._time_dependent :
 				info["stop_training"] = avg_reward > self._threshold
