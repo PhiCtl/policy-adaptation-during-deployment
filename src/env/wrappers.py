80ed8347f0e24d3513c94beec47afe8950ede6ad
PhiCtl <philippine.descourtils@epfl.ch>
2022-04-29 08:43:10 +0200

    gravity gradual

--- src/env/wrappers.py
+++ src/env/wrappers.py
@@ -121,8 +121,8 @@ class ColorWrapper(gym.Wrapper):
 
 	def modify_physics_model(self):
 		_env = self._get_dmc_wrapper()
-		self._change = np.abs(self._change - 2)
-		_env.physics.model.opt.gravity[0:2] = self._change
+		self._change = (self._change + 0.1) % 3
+		_env.physics.model.opt.gravity[1] = self._change
 
 	def get_state(self):
 		return self._get_state()
