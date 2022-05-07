24bbd70ecae2eee89b4a17eca7360ca41d86c19c
PhiCtl <philippine.descourtils@epfl.ch>
2022-05-07 10:38:42 +0200

    added bca to compare with reload on color changes

--- src/catastrophic_forgetting.py
+++ src/catastrophic_forgetting.py
@@ -12,6 +12,7 @@ from env.wrappers import make_pad_env
 from agent.agent import make_agent
 from utils import get_curl_pos_neg, AdaptRecorder
 from eval import evaluate, init_env
+from eval_BCA import prepare_BCA
 
 def compare_agents(args, agent, envs, recorder, video, exp_type, eval_fct = evaluate, reload=True) :
 
@@ -27,7 +28,7 @@ def compare_agents(args, agent, envs, recorder, video, exp_type, eval_fct = eval
         pad_reward, std = eval_fct(envs, agent, args, video, recorder, adapt=True, reload=reload, exp_type=exp_type)
         print('pad reward:', int(pad_reward), ' +/- ', int(std))
 
-def evaluate_seq(envs, agent, args, video, recorder, exp_type, adapt=False, reload = True) :
+def evaluate_seq(envs, agent, args, video, recorder, exp_type, clone=None, buffer=None, adapt=False, reload = True, bca=False) :
 
     print(f'----Evaluating a sequence of environments : {[env._mode for env in envs]}----')
     if adapt :
@@ -35,12 +36,14 @@ def evaluate_seq(envs, agent, args, video, recorder, exp_type, adapt=False, relo
     else :
         print(f'Non-adapting agent deployment of {args.work_dir} for {args.pad_num_episodes} episodes)')
     print(f'With pre-trained reload' if reload else f'Without pre-trained reload')
+    print(f'With bca' if bca else f'Without bca')
     print("-"*60)
 
+    assert(not (bca and reload)), "Either reload or bca mode is allowed"
     episode_rewards = []
 
     def run_episode(env):
-        if reload :
+        if reload:
             ep_agent = deepcopy(episode_agent)
         else :
             ep_agent = episode_agent
@@ -56,7 +59,7 @@ def evaluate_seq(envs, agent, args, video, recorder, exp_type, adapt=False, relo
             # Take step
             with utils.eval_mode(ep_agent):
                 action = ep_agent.select_action(obs)
-            next_obs, reward, done, info, change = env.step(action, rewards)
+            next_obs, reward, done, info, change, _ = env.step(action, rewards)
             ep_rew += reward
             recorder.update(change, reward)
 
@@ -68,6 +71,10 @@ def evaluate_seq(envs, agent, args, video, recorder, exp_type, adapt=False, relo
             # Adapt using inverse dynamics prediction
             losses.append(ep_agent.update_inv(utils.random_crop(batch_obs), utils.random_crop(batch_next_obs),
                                               batch_action))
+
+            if bca:
+                # Adapt using KL divergence loss and train Actor-Critic network
+                agent.update_actor_and_alpha(obs, bca_loss=bca, buffer=buffer, clone=clone)
 
             video.record(env, losses)
             obs = next_obs
@@ -101,17 +108,35 @@ def main(args):
     video_dir = utils.make_dir(os.path.join(args.work_dir, 'video'))
     video = VideoRecorder(video_dir if args.save_video else None, height=448, width=448)
 
+    # Initialize environments
+    training_env = make_pad_env(
+        domain_name=args.domain_name,
+        task_name=args.task_name,
+        seed=args.seed,
+        episode_length=args.episode_length,
+        action_repeat=args.action_repeat,
+        mode='train',
+        dependent=False)
+
     # Prepare agent
-    # Initialize environment
-    env = init_env(args)
     assert torch.cuda.is_available(), 'must have cuda enabled'
     cropped_obs_shape = (3 * args.frame_stack, 84, 84)
     agent = make_agent(
         obs_shape=cropped_obs_shape,
-        action_shape=env.action_space.shape,
+        action_shape=training_env.action_space.shape,
         args=args
     )
     agent.load(model_dir, args.pad_checkpoint)
+    clone = deepcopy(agent)
+
+    # Collect observations from source environment
+    replay_buffer = utils.ReplayBuffer(
+        obs_shape=training_env.observation_space.shape,
+        action_shape=training_env.action_space.shape,
+        capacity=args.train_steps,
+        batch_size=args.pad_batch_size
+    )
+    prepare_BCA(training_env, clone, replay_buffer, args.pad_num_episodes)
 
     # Recorder
     recorder = AdaptRecorder(args.work_dir, args.mode)
@@ -122,13 +147,13 @@ def main(args):
     # How does agent behave when deployed successively in different environment and then back in the training environment
     args.mode = 'color_easy'
     env_color_easy = init_env(args)
-    envs = [env_color_easy, env]
+    envs = [env_color_easy, training_env]
     evaluate_seq(envs, agent, args, video, recorder, adapt=True, reload=True, exp_type="reloaded")
 
 
     env_color_easy = init_env(args)
-    envs = [env_color_easy, env]
-    evaluate_seq(envs, agent, args, video, recorder, adapt=True, reload=False, exp_type="not_reloaded")
+    envs = [env_color_easy, training_env]
+    evaluate_seq(envs, agent, args, video, recorder, buffer=replay_buffer, clone=clone, adapt=True, reload=False, exp_type="bca", bca=True)
 
     # How does it behave if deployed successively in different envt with and without weight reloading ?
     # Evaluate agent with weight reloading
