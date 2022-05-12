af804918305e9fa21c68585206be852292d86fbd
PhiCtl <philippine.descourtils@epfl.ch>
2022-05-12 15:10:31 +0200

    reformat recorder

--- src/utils.py
+++ src/utils.py
@@ -45,11 +45,13 @@ class Recorder(object):
 
 class AdaptRecorder(Recorder):
 
-    def __init__(self, save_dir, type):
+    def __init__(self, save_dir, type, action=False):
         super().__init__(save_dir, type)
         self.changes_tot, self.changes = [], []
         self.rewards_tot, self.rewards = [], []
-        self.actions_tot, self.actions = [], []
+        self.actions_tot, self.actions = None, None
+        if action:
+            self.actions_tot, self.actions = [], []
 
     def reset(self):
         self.changes, self.rewards, self.actions = [], [], []
@@ -57,26 +59,31 @@ class AdaptRecorder(Recorder):
     def update(self, change, reward, action):
         self.changes.append(change)
         self.rewards.append(reward)
-        self.actions.append(action)
+        if self.actions :
+            self.actions.append(action)
 
     def end_episode(self):
         self.changes_tot.append(self.changes)
         self.rewards_tot.append(self.rewards)
-        self.actions_tot.append(self.actions)
+        if self.actions_tot:
+            self.actions_tot.append(self.actions)
 
         self.reset()
 
     def save(self, file_name, adapt):
         self.rewards_tot = np.array(self.rewards_tot).transpose()
         self.changes_tot = np.array(self.changes_tot).transpose()
-        self.actions_tot = np.array(self.actions_tot).transpose()
         df_r = pd.DataFrame(self.rewards_tot, columns=[f'episode_{i}_reward' for i in range(self.rewards_tot.shape[1])])
         df_s = pd.DataFrame(self.changes_tot,
                             columns=[f'episode_{i}_{self._type}' for i in range(self.changes_tot.shape[1])])
-        df_a = pd.DataFrame(self.actions_tot,
-                            columns=[f'episode_{i}_action' for i in range(self.actions_tot.shape[1])])
-        df_med = df_r.join(df_s)
-        df_tot = df_med.join(df_a)
+
+        if self.actions_tot:
+            self.actions_tot = np.array(self.actions_tot).transpose()
+            df_a = pd.DataFrame(self.actions_tot,
+                                columns=[f'episode_{i}_action' for i in range(self.actions_tot.shape[1])])
+            df_r = df_r.join(df_a)
+        df_tot = df_r.join(df_s)
+        
         # Rename file and folders
         file_name += datetime.now().strftime("%H-%M-%S")
         file_name += self._type
