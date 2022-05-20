9d80e168125f6d2117001eb58f43b632ee10a3e7
PhiCtl <philippine.descourtils@epfl.ch>
2022-05-20 11:26:37 +0200

    excluded nodes and removed lr spe

--- scripts/sbatch_il_agent.sh
+++ scripts/sbatch_il_agent.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+#SBATCH --exclude=i[20,24-25,27-28,36-40,53-57,65]
 #SBATCH --nodes 1
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 1
 #SBATCH --mem 80G
 #SBATCH --time 11:00:00
-#SBATCH --account vita
 #SBATCH --gres gpu:1
 
 python3 src/imitation_learning.py \
@@ -14,10 +14,7 @@ python3 src/imitation_learning.py \
     --mode train \
     --use_inv \
     --num_shared_layers 8 \
-    --actor_lr 3e-4 \
-    --encoder_lr 3e-4 \
     --pad_checkpoint 500k \
-    --ss_lr 3e-4 \
     --seed 0 \
     --work_dir logs/cartpole_swingup/inv/0 \
     --save_model
