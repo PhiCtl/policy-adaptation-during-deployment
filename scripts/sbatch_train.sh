fadf0de9ffbbb7b26ffbc4c0d739561b67fe63f9
PhiCtl <philippine.descourtils@epfl.ch>
2022-05-20 14:59:06 +0200

    train for walker walk forces

--- scripts/sbatch_train.sh
+++ scripts/sbatch_train.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
+#SBATCH --exclude=i[20,24-25,27-28,36-40,53-57,65]
 #SBATCH --nodes 1
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 1
-#SBATCH --mem 48G
-#SBATCH --time 11:00:00
-#SBATCH --account vita
+#SBATCH --mem 100G
+#SBATCH --time 2-00:00:00
 #SBATCH --gres gpu:1
 
 python3 src/train.py \
-    --domain_name cheetah \
-    --task_name run \
+    --domain_name walker \
+    --task_name walk \
     --action_repeat 4 \
     --mode train \
     --use_inv \
     --num_shared_layers 8 \
-    --actor_lr 3e-4 \
-    --critic_lr 3e-4 \
-    --encoder_lr 3e-4 \
-    --ss_lr 3e-4 \
     --seed 0 \
-    --work_dir logs/cheetah_run/inv/0 \
-    --save_model
+    --work_dir logs/walker_walk/inv/0 \
+    --save_model \
+    --force_walker -1.0
