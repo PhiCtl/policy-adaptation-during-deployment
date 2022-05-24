be3af3451c09b107ecc1514e19c1047fc8be3707
StellaPetronio <62093435+StellaPetronio@users.noreply.github.com>
2022-05-24 15:05:21 +0200

    sbatch domain generic

---
+++ scripts/sbatch_domain_generic.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --exclude=i[20,24-25,27-28,36-40,53-57,65]
+#SBATCH --nodes 1
+#SBATCH --ntasks 1
+#SBATCH --cpus-per-task 1
+#SBATCH --mem 80G
+#SBATCH --time 11:00:00
+#SBATCH --gres gpu:1
+
+python3 src/domain_generic.py \
+    --domain_name cartpole \
+    --task_name swingup \
+    --action_repeat 8 \
+    --mode train \
+    --use_inv \
+    --num_shared_layers 8 \
+    --pad_checkpoint 500k \
+    --seed 0 \
+    --work_dir logs/cartpole_swingup \
+    --save_dir logs/domain_generic/cartpole_swingup 
