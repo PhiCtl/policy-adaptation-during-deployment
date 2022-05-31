a89bf75bc96d8fc0f665dd019f13669a45744bbc
StellaPetronio <62093435+StellaPetronio@users.noreply.github.com>
2022-05-31 11:23:46 +0200

    train walker walk force -2

--- scripts/sbatch_train.sh
+++ scripts/sbatch_train.sh
@@ -14,6 +14,6 @@ python3 src/train.py \
     --use_inv \
     --num_shared_layers 8 \
     --seed 0 \
-    --work_dir logs/walker_walk_0_-1/inv/0 \
-    --force_walker 1.0 \
+    --work_dir logs/walker_walk_0_-2/inv/0 \
+    --force_walker 2.0 \
     --save_model
