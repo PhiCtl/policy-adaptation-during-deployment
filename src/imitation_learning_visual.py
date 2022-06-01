4df3e7a775bfb19f41528bd42608b985fdb16688
PhiCtl <philippine.descourtils@epfl.ch>
2022-06-01 09:04:31 +0200

    refractored code + training with only mass 0.4 and 0.2

--- src/imitation_learning_visual.py
+++ src/imitation_learning_visual.py
@@ -1,18 +1,6 @@
-from turtle import forward
-import torch
-import os
 from tqdm import tqdm
-import torch.nn as nn
-import numpy as np
-from copy import deepcopy
-
-from video import VideoRecorder
 from arguments import parse_args
-from agent.agent import make_agent
-from agent.IL_agent_visual import make_il_agent_visual
-import utils
-from eval import init_env, evaluate
-from logger import Logger
+from utils_imitation_learning import *
 
 """
 This script trains Imitation learning agents from RL experts trained on different domains (different by their dynamics
@@ -24,212 +12,15 @@ shared encoder, as input to SS and actor heads
 """
 
 
-def evaluate_agent(agent, env, args, exp_type="", buffer=None, step=None, adapt=False,
-                   feat_analysis=False, L=None, video=None, recorder=None): # OK
-    """Evaluate agent on env, storing obses, actions and next obses
-    Params : - agent : IL agent
-             - env : env to evaluate this agent in
-             - args
-             - buffer : needed to train IL agent with a trajectory as input to the domain specific module"""
-
-    ep_rewards = []
-    obses, actions, feat_vects = [], [], []
-
-    for i in tqdm(range(args.num_rollouts)):
-        ep_agent = deepcopy(agent)
-        if video: video.init(enabled=True)
-        obs = env.reset()
-        done = False
-        episode_reward = 0
-        step = 0
-        rewards, losses = [], []
-
-        ep_agent.train()
-
-        while not done:
-
-            # Take a step
-
-            # Trajectory : (obs, act, obs, act, obs)
-            traj = None if buffer is None else buffer.sample_traj()
-            if feat_analysis :  feat_vects.append(ep_agent.extract_feat_vect(traj))
-
-            with utils.eval_mode(ep_agent):
-                action = ep_agent.select_action(obs, traj)
-            next_obs, reward, done, info, change, _ = env.step(action, rewards)
-
-
-            # Save data
-            episode_reward += reward
-            obses.append(obs)
-            actions.append(action)
-
-            # Adapt
-            if adapt and buffer is not None :
-                # Prepare batch of observations
-                batch_obs = utils.batch_from_obs(torch.Tensor(obs).cuda(), batch_size=args.pad_batch_size)
-                batch_next_obs = utils.batch_from_obs(torch.Tensor(next_obs).cuda(), batch_size=args.pad_batch_size)
-                batch_action = torch.Tensor(action).cuda().unsqueeze(0).repeat(args.pad_batch_size, 1)
-
-                b_o1 = utils.batch_from_obs(traj[0], batch_size=args.pad_batch_size)
-                b_o2 = utils.batch_from_obs(traj[2], batch_size=args.pad_batch_size)
-                b_o3 = utils.batch_from_obs(traj[4], batch_size=args.pad_batch_size)
-                b_a1 = traj[1].repeat(args.pad_batch_size, 1)
-                b_a2 = traj[3].repeat(args.pad_batch_size, 1)
-                
-                losses.append(ep_agent.update_inv(utils.random_crop(batch_obs), utils.random_crop(batch_next_obs),
-                                                  batch_action, [b_o1, b_a1, b_o2, b_a2, b_o3]))
-
-            if video: video.record(env, losses)
-            if recorder: recorder.update(change, reward)
-            if L and step:
-                L.log('eval/episode_reward', episode_reward, step)
-
-            obs = next_obs
-            step += 1
-
-        obses.append(obs) # Save last next obs
-        ep_rewards.append(episode_reward)
-        if video: video.save(f'{args.mode}_pad_{i}.mp4' if adapt else f'{args.mode}_eval_{i}.mp4')
-        if recorder: recorder.end_episode()
-
-    if L and step:
-        L.dump(step)
-    if recorder: recorder.save("performance_" + exp_type, adapt)
-
-    if feat_analysis :
-        return np.array(ep_rewards), obses, actions, feat_vects
-    return np.array(ep_rewards), obses, actions
-
-def collect_trajectory(RL_reference, env, args):
-    """Collect trajectory on domain with a reference RL agent
-    which has not been trained on the given domain"""
-
-    buffer = utils.TrajectoryBuffer(
-        obs_shape=env.observation_space.shape,
-        action_shape=env.action_space.shape,
-        capacity=10000,
-        batch_size=args.batch_size
-    )
-    _, obses, actions = evaluate_agent(RL_reference, env, args)
-    buffer.add_path(obses, actions)
-
-    return buffer
-
-
-def collect_expert_samples(agent, env, args, label): # OK
-    """Collect data samples for Imitation learning training
-       Args : - agent : expert RL agent trained on env
-              - env : dm_control environment
-              - args
-              - label : env specificity, eg. the cartmass, the opponent force 
-       """
-    # Create replay buffer with label
-    buffer = utils.SimpleBuffer(
-        obs_shape=env.observation_space.shape,
-        action_shape=env.action_space.shape,
-        capacity=args.train_steps,
-        batch_size=args.batch_size,
-        label=label
-    )
-
-    ep_rewards, obses, actions = evaluate_agent(agent, env, args)
-    buffer.add_path(obses, actions) # add policy rollout to buffer
-    return buffer, ep_rewards.mean(), ep_rewards.std()
-
-def relabel(obses, expert): # OK
-    """Relabel observations with expert agent for DAgger algorithm"""
-    with utils.eval_mode(expert):
-        actions_new = []
-        for obs in obses:
-            actions_new.append(expert.select_action(obs))
-    return actions_new
-
-def load_agent(label, action_shape, args): # OK
-    """Load RL agent model from directory"""
-
-    work_dir = args.work_dir + label # example : logs/cartpole_swingup + "_0_3"
-    L = Logger(args.save_dir + label, use_tb=True, config='il')
-    model_dir = os.path.join(work_dir, 'inv', '0', 'model') # logs/cartpole_swingup_0_3/inv/0/model
-    print(f'Load agent from {work_dir}')
-
-    # Prepare agent
-    assert torch.cuda.is_available(), 'must have cuda enabled'
-    cropped_obs_shape = (3 * args.frame_stack, 84, 84)
-    agent = make_agent(
-        obs_shape=cropped_obs_shape,
-        action_shape=action_shape,
-        args=args
-    )
-    agent.load(model_dir, args.pad_checkpoint)
-
-    return agent, L
-    
 def main(args):
 
-    labels = ["_0_4", "_0_2", "_0_25", "_0_3"]
+    labels = ["_0_4", "_0_2"]#, "_0_25", "_0_3"],
+    domains = [0.4, 0.2 ] #, 0.25, 0.3]
+    stats_il = {k: [] for k in labels}  # save score of Il agents
 
-    # 1. Define 4 envts : four different environments / domains that differ by the mass or the force
-    print("-"*60)
-    print("Define environment")
-    envs = []
-    masses = [] # eg. for cartpole [1, 0.1] : mass of the cart and mass of the pole
-    for mass in [0.4, 0.2, 0.25, 0.3]:
-        env = init_env(args, mass)
-        masses.append(env.get_masses())
-        envs.append(env)
-
-    # 2. Load expert agents + reference agent
-    print("-" * 60)
-    print("Load experts")
-    experts = []
-    loggers = []
-    for label in labels:
-        # All envs have should have the same action space shape
-        agent, logger = load_agent(label, envs[0].action_space.shape, args)
-        experts.append(agent)
-        loggers.append(logger)
-    # Load reference agent
-    ref_expert, _ = load_agent("", envs[0].action_space.shape, args)
-
-    # 3. Collect samples from 4 RL agents
-    print("-" * 60)
-    print("Fill in buffers")
-    buffers = [] # save data for IL
-    stats_expert = dict() # save score of trained RL agents on corresponding environments
-    stats_il = {k:[] for k in labels} # save score of Il agents
-
-    # 4. Initialize buffers by collecting experts data and collect their performance in the meantime
-
-    # 4.a We have 1 buffer per (env, RL_expert)
-    for expert, force, env in zip(experts, labels, envs) :
-        buffer, mean, std = collect_expert_samples(expert, env, args, force)
-        buffers.append(buffer)
-        stats_expert[force] = [mean, std]
-
-    # 4.b Collect trajectories from ref RL agent on different domains
-    trajs_buffers = []
-    for env in envs:
-        trajs_buffers.append(collect_trajectory(ref_expert, env, args))
-
-    # 5. Create IL agents
-    print("-" * 60)
-    print("Create IL agents")
-    il_agents = []
-    cropped_obs_shape = (3 * args.frame_stack, 84, 84)
-
-    for mass in masses:
-        il_agent = make_il_agent_visual(
-            obs_shape=cropped_obs_shape,
-            action_shape=envs[0].action_space.shape,
-            args=args)
-        il_agents.append(il_agent)
-
-    # Share domain generic part between agents
-    il_agents_tied = [il_agents[0]]
-    for il_agent in il_agents[1:]:
-        il_agent.tie_agent_from(il_agents[0])
-        il_agents_tied.append(il_agent)
+    il_agents_train, experts, envs, _, buffers, trajs_buffers, stats_expert = setup(args,
+                                                                                 labels=labels,
+                                                                                 domains=domains)
 
     # 6. Train the four IL agents with DAgger algorithm
     print("-" * 60)
@@ -245,12 +36,12 @@ def main(args):
             preds, pred_invs, gts, losses = [], [], [], 0
 
             # Forward pass sequentially for all agents
-            for agent, buffer, traj_buffer, mass, L in zip(il_agents_tied, buffers, trajs_buffers, masses, loggers):
+            for agent, buffer, traj_buffer in zip(il_agents_train, buffers, trajs_buffers):
 
                 # sample a batch of obs, action, next_obs and traj = [obs1, act1, obs2, act2, obs3]
                 obs, action, next_obs = buffer.sample()
                 traj = traj_buffer.sample()
-                action_pred, action_inv, loss = agent.predict_action(obs, next_obs, traj, action, L=L, step=step)
+                action_pred, action_inv, loss = agent.predict_action(obs, next_obs, traj, action)
 
                 preds.append(action_pred) # Action from actor network
                 pred_invs.append(action_inv) # Action from SS head
@@ -260,16 +51,16 @@ def main(args):
             # Backward pass
             losses.backward()
 
-            for agent in il_agents_tied:
+            for agent in il_agents_train:
                 agent.update()
 
         # b. Evaluate - Perform IL agent policy rollouts
         print("\n\n********** Evaluation and relabeling %i ************" % it)
 
-        for agent, expert, logger, env, buffer, traj_buffer, mass in zip(il_agents_tied, experts, loggers, envs, buffers, trajs_buffers, labels):
+        for agent, expert, env, buffer, traj_buffer, mass in zip(il_agents_train, experts, envs, buffers, trajs_buffers, labels):
 
             # Evaluate agent on envt
-            rewards, obses, actions = evaluate_agent(agent, env, args, buffer=traj_buffer, L=logger, step=it) # change to traj_buffer
+            rewards, obses, actions, _ = evaluate_agent(agent, env, args, buffer=traj_buffer)
             # Save intermediary score
             stats_il[mass].append([rewards.mean(), rewards.std()])
             print(f'Performance of agent on mass {mass} : {rewards.mean()} +/- {rewards.std()}')
@@ -281,54 +72,23 @@ def main(args):
 
         # c. Save partial model
         if it % 2 == 0 :
-            for agent, label in zip(il_agents_tied, labels):
+            for agent, label in zip(il_agents_train, labels):
                 save_dir = utils.make_dir(os.path.join(args.save_dir, label, 'model'))
                 agent.save(save_dir, it)
 
 
     # 7. Save IL agents
-    for agent, label in zip(il_agents_tied, labels):
+    for agent, label in zip(il_agents_train, labels):
         save_dir = utils.make_dir(os.path.join(args.save_dir, label, 'model'))
         agent.save(save_dir, "final")
 
     # 8. Evaluate expert vs IL
     for label in labels:
         print("-" * 60)
-        print(f'Force of {label}')
+        print(f'Mass of {label}')
         print(f'Expert performance : {stats_expert[label][0]} +/- {stats_expert[label][1]}')
         print(f'Imitation learning agent with dagger performance : {stats_il[label][-1][0]} +/- {stats_il[label][-1][1]}')
 
-def test_agents(args):
-
-    # Load env for a given mass
-    envs = []
-    masses = []
-    #forces = []
-    for label in [0.25]: #, 0.2, 0.3, 0.4] :#[-1,-2,-3]:
-        env = init_env(args, label)
-        #forces.append(env.get_forces())
-        masses.append(env.get_masses())
-        envs.append(env)
-
-    # Build traj buffers
-    traj_buffers = []
-    ref_expert, _ = load_agent("", envs[0].action_space.shape, args)
-    for env in envs:
-        traj_buffers.append(collect_trajectory(ref_expert, env, args))
-
-    #for label, force, traj_buffer, env in zip(["_0_-1", "_0_-1", "_0_-3"], forces, traj_buffers, envs):
-    for label, mass, traj_buffer, env in zip(["_0_25"], masses, traj_buffers, envs):
-        # Load IL agent
-        cropped_obs_shape = (3 * args.frame_stack, 84, 84)
-        il_agent = make_il_agent_visual(
-            obs_shape=cropped_obs_shape,
-            action_shape=envs[0].action_space.shape,
-            args=args)
-        load_dir = utils.make_dir(os.path.join(args.save_dir, label, 'model'))
-        il_agent.load(load_dir, "final")
-        # Evaluate agent
-        reward, _, _ = evaluate_agent(il_agent, env, args, buffer=traj_buffer)
-        print('non adapting reward:', int(reward.mean()), ' +/- ', int(reward.std()), ' for label ', label)
 
 if __name__ == '__main__':
     args = parse_args()
