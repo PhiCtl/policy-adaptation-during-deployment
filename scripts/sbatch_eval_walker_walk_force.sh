a6f5101a993cf9f3fdfa47ec23db2c0d3dd5de25
StellaPetronio <62093435+StellaPetronio@users.noreply.github.com>
2022-06-03 11:16:48 +0200

    correct typo

--- scripts/sbatch_eval_walker_walk_force.sh
+++ scripts/sbatch_eval_walker_walk_force.sh
@@ -15,7 +15,8 @@ python3 src/eval.py \
 	--use_inv \
 	--num_shared_layers 8 \
 	--seed 0 \
-	--work_dir logs/walker_walk/inv/0 \
+	--force_walker -1 \
+	--work_dir logs/walker_walk_-1/inv/0 \
 	--pad_checkpoint 500k \
 	--pad_num_episodes 100 \
 	--episode_length 1000
