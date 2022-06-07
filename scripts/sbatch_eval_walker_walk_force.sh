98f3ea856fe43b1798c19826b45561d541cd9f46
StellaPetronio <62093435+StellaPetronio@users.noreply.github.com>
2022-06-07 08:55:12 +0200

    force -1

--- scripts/sbatch_eval_walker_walk_force.sh
+++ scripts/sbatch_eval_walker_walk_force.sh
@@ -16,6 +16,7 @@ python3 src/eval.py \
 	--num_shared_layers 8 \
 	--seed 0 \
 	--work_dir logs/walker_walk/inv/0 \
+	--force_walker -1 /
 	--pad_checkpoint 500k \
 	--pad_num_episodes 100 \
 	--episode_length 1000
