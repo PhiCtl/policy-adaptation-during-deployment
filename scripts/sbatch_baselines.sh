2ef016b9331812d929fb944f428a0de5ff11cbe5
PhiCtl <philippine.descourtils@epfl.ch>
2022-06-09 09:44:08 +0200

    baseline script

--- scripts/sbatch_baselines.sh
+++ scripts/sbatch_baselines.sh
@@ -4,18 +4,20 @@
 #SBATCH --cpus-per-task 1
 #SBATCH --mem 48G
 #SBATCH --time 07:30:00
-#SBATCH --account vita
 #SBATCH --gres gpu:1
 
 python3 src/eval.py \
-	--domain_name walker \
-	--task_name walk \
-	--action_repeat 4 \
+	--domain_name cartpole \
+	--task_name swingup \
+	--action_repeat 8 \
 	--mode train \
 	--use_inv \
 	--num_shared_layers 8 \
 	--seed 0 \
-	--work_dir logs/walker_walk/inv/0 \
+	--work_dir logs/cartpole_swingup/inv/0 \
 	--pad_checkpoint 500k \
+	--dependent \
+	--window 300 \
+	--cart_mass 0.4 \
 	--pad_num_episodes 100 \
-	--episode_length 1000
+	--episode_length 10000
