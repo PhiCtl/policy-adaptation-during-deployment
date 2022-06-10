1add95bd8009fed4ce52bfa20ad9f0907196bbec
PhiCtl <philippine.descourtils@epfl.ch>
2022-06-10 08:56:27 +0200

    gt mode

--- src/tta_il_agent.py
+++ src/tta_il_agent.py
@@ -124,7 +124,7 @@ def main(args):
 
     for lr in [0.0001, 0.001, 0.005, 0.01, 0.05, 0.1]:
         print("Learning rate :", lr)
-        seeds_summary_visual(args, lr=lr) # change to seeds_summary(args, lr=lr) if needed
+        seeds_summary(args, lr=lr) # change to seeds_summary(args, lr=lr) if needed
 
 def test_agents(args):
 
