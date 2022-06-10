81133b19a12e101029f6b004fc62386983d80890
PhiCtl <philippine.descourtils@epfl.ch>
2022-06-10 11:22:58 +0200

    corrected typo

--- src/imitation_learning_visual.py
+++ src/imitation_learning_visual.py
@@ -106,7 +106,7 @@ def main(args):
     for agent, env, traj_buffer, label in zip(il_agents_train, envs, trajs_buffers,
                                                              labels):
         # Evaluate agent on envt
-        rewards, obses, actions, _ = evaluate_agent(agent, env, args, buffer=traj_buffer)
+        rewards, obses, actions = evaluate_agent(agent, env, args, buffer=traj_buffer)
         # Save intermediary score
         stats_il[label].append([rewards.mean(), rewards.std()])
         print(f'Performance of agent on force {label} : {rewards.mean()} +/- {rewards.std()}')
