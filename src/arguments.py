13575a30bb7570fbfd92585b496d7f70730662d7
PhiCtl <philippine.descourtils@epfl.ch>
2022-06-17 09:10:54 +0200

    added more comments

--- src/arguments.py
+++ src/arguments.py
@@ -8,18 +8,18 @@ def parse_args():
 	parser = argparse.ArgumentParser()
 
 	# environment
-	parser.add_argument('--domain_name', default='walker')
-	parser.add_argument('--task_name', default='walk')
+	parser.add_argument('--domain_name', default='walker') # Can be changed to cartpole
+	parser.add_argument('--task_name', default='walk')# Can be changed to swingup
 	parser.add_argument('--frame_stack', default=3, type=int)
 	parser.add_argument('--action_repeat', default=4, type=int) # 8 for cartpole
 	parser.add_argument('--episode_length', default=1000, type=int)
-	parser.add_argument('--mode', default='train', type=str) # See below for all the possible options
+	parser.add_argument('--mode', default='train', type=str) # See at the bottom for all the possible options
 	# To build environment baselines
-	parser.add_argument('--dependent', default=False, action='store_true')
-	parser.add_argument('--window', default=100, type=int)
+	parser.add_argument('--dependent', default=False, action='store_true') # Used for eval_domain.sh experiments
+	parser.add_argument('--window', default=100, type=int) # Time window during which the domain stays constant at evaluation
 	# Domain specific
-	parser.add_argument('--cart_mass', default=None, type=float)
-	parser.add_argument('--force_walker', default=None, type=float)
+	parser.add_argument('--cart_mass', default=None, type=float) # Default is 1
+	parser.add_argument('--force_walker', default=None, type=float) # Default is 0
 	
 	# agent
 	parser.add_argument('--init_steps', default=1000, type=int)
@@ -33,17 +33,17 @@ def parse_args():
 	parser.add_argument('--eval_episodes', default=10, type=int)
 
 	# imitation learning
-	parser.add_argument('--num_rollouts', default=100, type=int)
+	parser.add_argument('--num_rollouts', default=100, type=int) # Number of policy evaluations
 	parser.add_argument('--n_iter', default=10, type=int) # Number of DAgger iterations
-	parser.add_argument('--il_steps', default=5000, type=int) # Number of training steps @ each iteration
+	parser.add_argument('--il_steps', default=5000, type=int) # Number of training steps @ each DAgger iteration
 	parser.add_argument('--dynamics_output_shape', default=10, type=int) # Latent dynamics shape
 	# Imitation learning at test time
 	parser.add_argument('--domain_test', default=0.35, type=float) # Domain on which we test the agent
-	parser.add_argument('--label', default="_0_3", type=str) # Reference to the agent model we load
-	parser.add_argument('--domain_training', default=0.3, type=float) # Domain on which the agent was trained
+	parser.add_argument('--label', default="_0_3", type=str) # Directory reference to the agent model we load
+	parser.add_argument('--domain_training', default=0.3, type=float) # Domain on which the agent was trained (should be the same as label)
 	# For ground truth input based agents only
-	parser.add_argument('--rd', default=False, action='store_true') # If we initialize the feature vector at random
-	parser.add_argument('--visual', default=False, action='store_true')
+	parser.add_argument('--rd', default=False, action='store_true') # If we initialize the feature vector at random - Not used
+	parser.add_argument('--visual', default=False, action='store_true') # For trajectory input based agent
 	parser.add_argument('--il_lr', default=1e-3, type=float) # learning rate for test time adaptation
 
 	# critic
